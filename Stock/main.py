5351951d1626d7f99b10db624bd989df71341f9f
KY <moapace@gmail.com>
2018-05-30 15:21:14 +0800

    Update

--- Stock/main.py
+++ Stock/main.py
@@ -1,1 +1,9 @@
-print ("Hello")
+from __future__ import print_function
+
+print ("Hello world")
+
+DATA = ["one", "two", "three"]
+
+for filename in DATA:
+    print ("Loading[",DATA.index(filename), end='')
+    print ("] is ", filename)
